5e527d65ae10187589dabf530b95241e388c160f
manuel <mmolmil158@g.educaand.es>
2024-06-19 15:43:44 +0200

    RA5, propuesta ejercicio 3

--- src/main/java/RA5/ejercicio2/Helper.java
+++ src/main/java/RA5/ejercicio2/Helper.java
@@ -1,11 +1,14 @@
 package RA5.ejercicio2;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Helper {
     public static List<Location> getLocation(String path) throws IOException {
@@ -35,11 +38,35 @@ public class Helper {
         return locations;
     }
 
+    public static void write10Location(String path, List<Location> locations) throws IOException {
+        //Crear el path
+        Path path1 = Path.of(path);
+
+        //obtenemos un valor aleatorio y a partir de él cogemos diez valores
+        Random random = new Random();
+        int position = random.nextInt(locations.size());
+        //ejemplo una lista de 100, sale el 10, cogemos 10, 11, 12 ... 19
+        //ejemplo una lista de 100, sale el 98, cogemos 98, 99, 0, 1 ... 7
+        //en el caso que superemos las posiciones, ejemplo 100 -> 0 101 -> 1
+        //si supereamo la última posición usamos 100 % locations.size()
+        //Creamos el fichero de salida y escribimos la primera posción:
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 10; i++)
+            stringBuilder.append(locations.get((position + i) % locations.size()))
+                    .append('\n');
+        Files.writeString(path1, stringBuilder.toString().
+                        substring(0, stringBuilder.toString().length() - 1),
+                StandardOpenOption.CREATE);
+    }
+
     public static void main(String[] args) {
         //leemos y mostramos los datos en consola
         try {
             List<Location> locations = Helper.getLocation("files/input/locations.csv");
             locations.forEach(System.out::println);
+            String path = "files/output/diez.csv";
+            Helper.write10Location(path, locations);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
---
+++ src/main/java/RA5/ejercicio3.java
@@ -0,0 +1,13 @@
+package RA5;
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+
+    }
+    //método para leer fichero, devuelve el List<String>
+    //métdo le pasamos List<String> y buscamos palabras en cada línea y devolvemo el nº
+    //método le pasamos List<String>, si tiene cinco líneas, eliges una al azar y haces split
+    //y escoges aleatoriamente una palabra
+    //en el main, crear un StringBuilder con el informe
+    //luego un método para escribir (files/output/analisis_fichero.txt)
+}
