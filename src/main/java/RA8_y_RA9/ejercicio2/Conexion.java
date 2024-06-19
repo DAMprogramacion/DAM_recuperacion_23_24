05999b02c196ebc803ad019f1d50adf84683ad7f
manuel <mmolmil158@g.educaand.es>
2024-06-19 16:43:40 +0200

    RA8 y RA9 nuevo DAO y CRUD

--- src/main/java/RA8_y_RA9/ejercicio2/Conexion.java
+++ src/main/java/RA8_y_RA9/ejercicio2/Conexion.java
@@ -1,4 +1,27 @@
 package RA8_y_RA9.ejercicio2;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
+    private static Conexion conexion;
+    private Connection connection;
+    private Conexion() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:sqlite:DB/data1.db");
+    }
+    public static Conexion getInstance() throws SQLException {
+        if (conexion == null)
+            conexion = new Conexion();
+        return conexion;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        System.out.println(getInstance().getConnection());
+        System.out.println(getInstance().getConnection());
+    }
 }
